1be818f566db971984e1520caf52792d7591a746
Enrique Hidalgo <koko.freelance@gmail.com>
2021-07-13 12:24:00 +0100

    Ups, me voy pa mi rama

--- userScore.js
+++ userScore.js
@@ -21,12 +21,14 @@ let player4 = {
   timeScore: 32,
 };
 
+let username = "Manu";
+
 // ----------- TO FIND THE WINNER ----------
 
 let scores = [];
-let players = [player1, player2, player3, player4];
-for (let index = 0; index < players.length; index++) {
-  scores.push(players[index].timeScore);
+let winners = [player1, player2, player3, player4];
+for (let index = 0; index < winners.length; index++) {
+  scores.push(winners[index].timeScore);
 }
 
 let scoreNumbers = Math.min(...scores);
@@ -34,40 +36,25 @@ let scoreNumbers = Math.min(...scores);
 // ------- PA METERLO EN EL ARRAY ------
 
 let player5 = new Object();
-player5.name = username.value;
-player5.timeScore = 200;
-players.push(player5);
-console.log(players);
+player5.name = username;
+player5.timeScore = 0;
+winners.push(player5);
 
 // ----------- TO PAINT THE CURRENT PLAYER & RANKING-------------
 
-let currentPlayer = document.querySelector("#currentPlayer");
-currentPlayer.innerHTML = player5.name;
+showScore();
 
-let winner1 = document.querySelector("#winner1");
-winner1.innerHTML = player1.name;
-let winner2 = document.querySelector("#winner2");
-winner2.innerHTML = player2.name;
-let winner3 = document.querySelector("#winner3");
-winner3.innerHTML = player3.name;
-let winner4 = document.querySelector("#winner4");
-winner4.innerHTML = player4.name;
+function showScore() {
+  let scoreScreen = document.getElementById("scoreScreen");
+  let playerInfo = Object.keys(player);
 
-// ------------- TO PAINT THE TIME SCORES ------------
-
-let first = document.getElementById("firstScore");
-first.innerHTML = 8 + " seconds.";
-
-let second = document.getElementById("secondScore");
-second.innerHTML = 13 + " seconds.";
-
-let third = document.getElementById("thirdScore");
-third.innerHTML = 25 + " seconds.";
-
-let fourth = document.getElementById("fourthScore");
-fourth.innerHTML = 38 + " seconds.";
-
-let current = document.getElementById("currentScore");
-current.innerHTML = "Currently Playing...";
-
-let nextPlayer = username.value;
+  for (let i = 0; i < winners.length; i++) {
+    for (let index = 0; index < playerInfo.length; index++) {
+      let detail = playerInfo[index];
+      // console.log(winners[i][detail]);
+      let score = document.createElement("div");
+      score.innerHTML = winners[i][detail];
+      scoreScreen.appendChild(score);
+    }
+  }
+}
