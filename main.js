13923db4f0c725b61da4cd0782f8019a00034924
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-07-13 10:52:54 +0100

    Index css

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-// Salva - Hangman
-// Kike - Oooooh!
-//Paris Arcos
-//Alberto
-
-console.log("funciono");
+    <script src="assets/js/variables.js" ></script>
+    <script src="assets/js/functions.js" ></script>
+    <script src="assets/js/user-form.js" ></script>
+    <script src="assets/js/main.js" ></script>
+    <script src="assets/js/shipping-form.js" ></script>
+    <script src="assets/js/finish-form.js" ></script>
+    <script src="assets/js/thank-you.js" ></script>
