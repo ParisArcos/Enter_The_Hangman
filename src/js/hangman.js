b23709196458d1a4649a573ac133286061f584ea
SalvaBandicoot <salvawebdg@gmail.com>
2021-07-14 12:20:34 +0200

    add startHangman function

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -5,6 +5,111 @@ let randomWord = words[Math.floor(Math.random() * words.length)]; //Get a random
 let letters = []; //To store the letters
 console.log(letters);
 
+//EnterTheHangman
+function startHangman() {
+  let hangmanTemplate = ` <div id="hangman">
+  <h1>Guess the word</h1>
+  <p>Muñequito</p>
+  <div id="wordContainer">
+    <div id="letterContainer"></div>
+  </div>
+  <div id="keyboardTop">
+  <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">Q</div>
+  </div>
+  <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">W</div>
+  </div>
+  <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">E</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">R</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">T</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">Y</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">U</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">I</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">O</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">P</div>
+  </div>
+  </div>
+  <div id="keyboardMid">
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">A</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">S</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">D</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">F</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">G</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">H</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">J</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">K</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">L</div>
+  </div>
+  </div>
+  <div id="keyboardBottom">
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">Z</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">X</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">C</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">V</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">B</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">N</div>
+  </div>
+    <div class="keyboardLetterContainer">
+    <div class="keyboardLetter">M</div>
+  </div>
+    <div class="keyboardLetterContainer">
+  </div>
+</div>
+</div>`;
+
+  contentArea.innerHTML = "";
+  contentArea.insertAdjacentHTML("beforeend", hangmanTemplate);
+
+  let insertHangman = document.getElementById("hangman").content;
+  let copyHangman = document.importNode(insertHangman, true);
+
+  contentArea.appendChild(copyHangman);
+}
+
 //Divides the random word
 function wordDivide(randomWord) {
   var wordArray = randomWord.split("");
