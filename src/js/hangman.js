73b39c365bf7d09cb339a613e81cdd6f251ae488
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-07-15 15:48:01 +0100

    cleanscores

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -294,7 +294,7 @@ function gameWin() {
   player5.timeScore = seconds;
   winners.push(player5);
   clearInterval(timeScoreboard);
-  cleanScores();
+  cleanScoreScreen();
   showScore();
   winTheHangman();
 }
